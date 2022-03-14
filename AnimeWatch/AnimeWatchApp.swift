460adb520a7b13b207eb3aa019ac4f998b8b7395
andrea <patini929@gmail.com>
2022-03-14 21:34:34 +0100

    Initial Commit

---
+++ AnimeWatch/AnimeWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeWatchApp.swift
+//  AnimeWatch
+//
+//  Created by Andrea on 14/03/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimeWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
